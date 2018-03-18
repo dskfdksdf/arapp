cd248676cb3c5a31b564f4fa5a84c6bc9aa11554
bob <3029239061@qq.com>
2018-03-18 15:53:00 +0800

    数据库文件

---
+++ arapp.sql
@@ -0,0 +1,35 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50716
+Source Host           : localhost:3306
+Source Database       : arapp
+
+Target Server Type    : MYSQL
+Target Server Version : 50716
+File Encoding         : 65001
+
+Date: 2018-03-18 15:48:36
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '毛奇志', '123456');
+INSERT INTO `user` VALUES ('2', '杨昊凡', '123456');
+INSERT INTO `user` VALUES ('3', '牛萌', '123456');
+INSERT INTO `user` VALUES ('4', '刘立', '123456');
